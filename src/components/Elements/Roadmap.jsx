5be1ddbe89bc50add4a167e47a7ca02b936ceb90
hamza <hamzasyed99921@gmail.com>
2022-11-01 10:30:58 +0500

    update

--- src/components/Elements/Roadmap.jsx
+++ src/components/Elements/Roadmap.jsx
@@ -3,7 +3,100 @@ import React from "react";
 const Roadmap = () => {
   return (
     <>
-
+      {/* <div className="roadmap">
+      <div className="raodmap-container">
+        <div className="raodmap-section active">
+          <div className="raodmap-section-node ">
+            <div className="raodmap-section-branch">
+              <div className="raodmap-text">
+                <div className="raodmap-text-i">
+                  <div className="raodmap-text-title">Highfield</div>
+                  <div className="raodmap-text-content">
+                    <ul className="list-minus">
+                      <li>Protocol Development</li>
+                      <li>Documentation</li>
+                      <li>Testnet</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="raodmap-section long ">
+          <div className="raodmap-section-year" />
+          <div className="raodmap-section-node">
+            <div className="raodmap-section-branch">
+              <div className="raodmap-text">
+                <div className="raodmap-text-i">
+                  <div className="raodmap-text-content">
+                    <ul className="list-minus">
+                      <li>Token Generating Event</li>
+                      <li>Audit</li>
+                    </ul>
+                    <div className="raodmap-text-title">Heave</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="raodmap-section  long">
+          <div className="raodmap-section-year" />
+          <div className="raodmap-section-node">
+            <div className="raodmap-section-branch">
+              <div className="raodmap-text">
+                <div className="raodmap-text-i">
+                  <div className="raodmap-text-title">Frigate</div>
+                  <div className="raodmap-text-content">
+                    <ul className="list-minus">
+                      <li>Protocol Launch</li>
+                      <li>Governance</li>
+                      <li>Partnerships</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="raodmap-section  long ">
+          <div className="raodmap-section-node">
+            <div className="raodmap-section-branch">
+              <div className="raodmap-text">
+                <div className="raodmap-text-i">
+                  <div className="raodmap-text-content">
+                    <ul className="list-minus">
+                      <li>Maximizer Pools</li>
+                      <li>Strategy Builder</li>
+                    </ul>
+                    <div className="raodmap-text-title">Armor</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="raodmap-section  long">
+          <div className="raodmap-section-node">
+            <div className="raodmap-section-branch">
+              <div className="raodmap-text">
+                <div className="raodmap-text-i">
+                  <div className="raodmap-text-title">Ensign</div>
+                  <div className="raodmap-text-content">
+                    <ul className="list-minus">
+                      <li>Multi-Token Maximizer</li>
+                      <li>User Dashboard Analytics</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="raodmap-section long" />
+      </div>
+      </div> */}
     </>
   );
 };
